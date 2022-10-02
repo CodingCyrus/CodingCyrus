f79c945acead3005ddf78d829a44633ed2448850
CodingCyrus <cyrushc@bu.edu>
2022-10-01 20:59:31 -0400

    Adding 3d Text, Earth, Spinning Moon Object

--- CodingCyrus_Portfolio_2022/src/script.js
+++ CodingCyrus_Portfolio_2022/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
@@ -20,21 +20,44 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xFFFFF7, 0.5)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
+// const directionalLight = new THREE.DirectionalLight(0x001eff, 0.3)
+// directionalLight.position.set(1, 0.25, 0)
+// scene.add(directionalLight)
+
+// const hemisphereLight = new THREE.HemisphereLight(0xffd300, 0x0000ff, 1)
+// scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xF4E99B, 0.5)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
+/**
+ * Scene Background
+ */
+scene.background = new THREE.CubeTextureLoader()
+    .setPath('textures/matcaps/')
+    .load([
+    'space_background.jpg',
+    'space_background.jpg',
+    'space_background.jpg',
+    'space_background.jpg',
+    'space_background.jpg',
+    'space_background.jpg'
+]);
 
 /**
  * Textures
  */
  const textureLoader = new THREE.TextureLoader()
  const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
+//  const matcapTexture = textureLoader.load('/textures/matcaps/9.png')
+const earthTexture = textureLoader.load('/textures/matcaps/sphere_textures/EarthWebGL.jpg')
+const moonTexture = textureLoader.load('/textures/matcaps/sphere_textures/moon.jpg')
 
 /**
  * Fonts
@@ -42,7 +65,13 @@ scene.add(pointLight)
  const fontLoader = new FontLoader()
 
  fontLoader.load(
-     '/fonts/helvetiker_bold.typeface.json',
+    //  '/fonts/helvetiker_bold.typeface.json',
+    // '/fonts/CyberFont Black_Regular.json',
+    // '/fonts/Blade Runner Movie Font_Regular.json',
+    // '/fonts/Cyberpunk_Regular.json',
+    // '/fonts/HACKED_Regular.json',
+    // '/fonts/Bugfast_Regular.json',
+    '/fonts/Digital Dare_Regular.json',
      (font) => 
      {
          console.log("loaded")
@@ -60,13 +89,34 @@ scene.add(pointLight)
                  bevelSegments: 5
              }
          )
+
+         const textGeometry2 = new TextGeometry(
+            'Welcome To',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
  
          textGeometry.center()
+         textGeometry2.center()
  
          const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture})
-         //Create Text Mesh
-         const text = new THREE.Mesh(textGeometry, material)
+         //Create and add to Scene CodingCyrus Text Mesh
+         const text = new THREE.Mesh(textGeometry, material);
+         text.position.y = 2.5;
          scene.add(text)
+         //Create and add to scene Welcome To Text Mesh
+         const text2 = new THREE.Mesh(textGeometry2, material);
+         text2.position.y = 3.5;
+         scene.add(text2)
      }
  )
 
@@ -75,36 +125,59 @@ scene.add(pointLight)
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
+// Materials
+const earthMaterial = new THREE.MeshStandardMaterial()
+// const material = new THREE.MeshNormalMaterial()
+// const material = new THREE.MeshBasicMaterial({map: sphereTexture})
+// const material = new THREE.MeshStandardMaterial()
+earthMaterial.metalness = 0.20
+earthMaterial.roughness = 0.50
+earthMaterial.map = earthTexture
+
+const moonMaterial = new THREE.MeshStandardMaterial()
+// const material = new THREE.MeshNormalMaterial()
+// const material = new THREE.MeshBasicMaterial({map: sphereTexture})
+// const material = new THREE.MeshStandardMaterial()
+moonMaterial.metalness = 0.20
+moonMaterial.roughness = 0.50
+moonMaterial.map = moonTexture
 
 // Objects
-const sphere = new THREE.Mesh(
+const earth = new THREE.Mesh(
+    new THREE.SphereGeometry(2, 32, 32),
+    earthMaterial
+)
+
+const moon = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
-    material
+    moonMaterial
 )
-sphere.position.x = - 1.5
+moon.position.x = -4
 
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
-)
+const moonObj = new THREE.Object3D();
+moonObj.add(moon)
+scene.add(moonObj)
 
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-    material
-)
-torus.position.x = 1.5
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(0.75, 0.75, 0.75),
+//     material
+// )
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(5, 5),
-    material
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
+// const torus = new THREE.Mesh(
+//     new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+//     material
+// )
+// torus.position.x = 1.5
 
-scene.add(sphere, cube, torus, plane)
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(5, 5),
+//     material
+// )
+// plane.rotation.x = - Math.PI * 0.5
+// plane.position.y = - 0.65
+
+scene.add(earth)
+// scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -134,9 +207,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.x = 2.5
+camera.position.y = 2
+camera.position.z = 7
 scene.add(camera)
 
 // Controls
@@ -163,13 +236,16 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+    earth.rotation.y = 0.5 * elapsedTime
+    moon.rotateY(0.004);
+    moonObj.rotateY(0.04);
 
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+    // cube.rotation.y = 0.1 * elapsedTime
+    // torus.rotation.y = 0.1 * elapsedTime
+
+    // sphere.rotation.x = 0.15 * elapsedTime
+    // cube.rotation.x = 0.15 * elapsedTime
+    // torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
